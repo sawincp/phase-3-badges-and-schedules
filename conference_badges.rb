0d9146518698f8effb110aaefb1a12815c0d6d35
Corey Sawin <sawincp@gmail.com>
2023-07-24 21:35:47 -0400

    commit tree 85a1af9daf7d63c1fe005fc680455cf68ca5120f

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map{|n| badge_maker(n)}
+end
+
+def assign_rooms(names)
+    names.map.with_index(1) do |name, room|
+         "Hello, #{name}! You'll be assigned to room #{room}!"
+    end
+end
+
+def printer(names)
+    batch_badge_creator(names).each do |badge|
+        puts badge
+    end
+    assign_rooms(names).each do |room|
+        puts room
+    end
+end
